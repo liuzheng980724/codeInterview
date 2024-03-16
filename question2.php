2209e90f1632ef3a216663f40c99fa874e559509
liuzheng980724 <mail@liuzheng.org>
2024-03-16 05:33:36 +0000

    Finish question2

--- question2.php
+++ question2.php
@@ -1,1 +1,13 @@
 <?php
+for ($i = 1; $i <= 1000; $i++) {
+
+    if (($i%3 == 0) AND ($i%5 == 0)) {
+        print ("foobar, ");
+    } else if(($i%3 == 0)) {
+        print ("foo, ");
+    } else if(($i%5 == 0)) {
+        print ("bar, ");
+    } else {
+        print("$i, ");
+    }
+}
